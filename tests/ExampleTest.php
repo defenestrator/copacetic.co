257e203f6eec59ee11b46863f99ebe8aa24d389f
Jeremy Anderson <jeremy@edos.io>
2016-09-03 20:51:21 -0600

    test and travis

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Laravel');
+             ->see('Copacetic - Blissful Software Delivery Consulting');
     }
 }
