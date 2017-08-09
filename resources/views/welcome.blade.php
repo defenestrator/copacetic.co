87a2bbe1e9a4e9daf04ea7c33fc7879f0b5f0772
Jeremy Anderson <jeremy@edos.io>
2017-08-08 22:30:21 -0600

    layouts are hell

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,17 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background: rgb(242,229,255);
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
-                height: 100vh;
                 margin: 0;
+                height: 100vh;
             }
 
             .full-height {
+                background: linear-gradient(rgb(255,250,240),rgb(242,229,255));
                 height: 100vh;
-                background: linear-gradient(rgb(255,250,240),rgb(242,229,255)),
-                            linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6));
             }
 
             .flex-center {
@@ -72,6 +71,9 @@
                 color:white;
             }
 
+            .links {
+                padding: .5em;
+            }
             .links > a {
                 color: #636b6f;
                 padding: 0 1em;
@@ -83,41 +85,26 @@
                 line-height:1.8rem;
             }
 
-            .promo-img {
-                opacity: .5;
-            }
-
             .form-group { padding-top:.4em; }
             .footer { font-size: 0.67rem; padding-top: .5rem; }
             .input { width: 304px; border-radius: 0.5em; line-height: 1.5rem;}
-            .converse, .success, .errors {  padding:.2rem .2rem; border-radius:0.5rem;  }
+            .success, .errors {  margin:0 auto; width: 304px; padding:.2rem 0; border-radius:0.5rem;  }
             .errors{ background: #f9f9c0; border: 1px solid #ffff00; }
             .message-list-item { text-align: left; list-style-type: none; }
             .success {background: #acf99f; border: 1px solid #2e5d2e; }
-            .message { }
+            .btn-big { width: 304px;}
             .g-recaptcha {display: inline-block;}
         </style>
         <script src="https://unpkg.com/vue"></script>
-        <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
                 <img src="/images/copacetic.co-logo-web.png"
                      class="promo-img"
-                     title="Copacetic - Blissful Software Delivery Consulting"
-                     alt="Copacetic - Blissful Software Delivery Consulting"
+                     title="Feel Good Software Delivery Consulting, Copacetic"
+                     alt="Feel Good Software Delivery Consulting, Copacetic."
                 />
                 <div class="links">
                     <a href="#development">Make</a>
@@ -165,16 +152,28 @@
                         </div>
                         <div class="form-group">
                             <button type="submit"
-                                    class="btn btn-primary"
+                                    class="btn btn-primary btn-big"
                             >
-                               HOWDY
+                               HOWDY!
                             </button>
                         </div>
 
                     </form>
                 </div>
                 <hr>
+                <div class="links">
+                    @if (Route::has('login'))
+                        <div class="footer links">
+                            @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                            @else
+                                <a href="{{ route('login') }}">Login</a>
+                                <a href="{{ route('register') }}">Register</a>
+                                @endauth
+                        </div>
+                    @endif
 
+                </div>
                 <div class="footer">
                     <p>&copy; Copacetic Media </p>
                     <p>{{ \Carbon\Carbon::now('America/Denver') }}</p>
