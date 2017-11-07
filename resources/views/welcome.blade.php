cab5c2fd212df8771df786acdda0497e65d8beee
Jeremy Anderson <jeremy@edos.io>
2017-11-07 11:56:48 -0700

    google analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,17 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107298455-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-107298455-2');
+        </script>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Feel Good Software Delivery</title>
 
         <!-- Styles -->
